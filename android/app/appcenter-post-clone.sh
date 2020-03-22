951902ef36a60de034e6adcf569a4eaf6edbe034
Andrew Kaiser <ndrwksr@gmail.com>
2020-03-22 15:07:12 -0500

    Updated build configuration

--- android/app/appcenter-post-clone.sh
+++ android/app/appcenter-post-clone.sh
@@ -23,4 +23,4 @@ chmod a+rx android/gradlew
 flutter build apk --release
 
 # copy the APK where AppCenter will find it
-mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-debug.apk $_
+mkdir -p android/app/build/outputs/apk/; mv build/app/outputs/apk/release/app-release.apk $_
